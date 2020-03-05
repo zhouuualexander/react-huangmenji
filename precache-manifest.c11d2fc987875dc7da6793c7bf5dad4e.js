068dbebc566887d8f80b3936579837a565ca7297
Zijian Zhou <zzhou3@ualberta.ca>
2020-03-05 16:29:37 -0700

    Updates

---
+++ precache-manifest.c11d2fc987875dc7da6793c7bf5dad4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a543f34f8e2a896d534b7f61f8a27c",
+    "url": "/react-huangmenji/index.html"
+  },
+  {
+    "revision": "df6b24a373f1ce7d6234",
+    "url": "/react-huangmenji/static/css/main.da7b87dc.chunk.css"
+  },
+  {
+    "revision": "f509e3a372571928da77",
+    "url": "/react-huangmenji/static/js/2.4499109b.chunk.js"
+  },
+  {
+    "revision": "a9cdcbe64e9e654566982e75c055afec",
+    "url": "/react-huangmenji/static/js/2.4499109b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6b24a373f1ce7d6234",
+    "url": "/react-huangmenji/static/js/main.80e3b3da.chunk.js"
+  },
+  {
+    "revision": "a83a77ca90784ea2b06d",
+    "url": "/react-huangmenji/static/js/runtime-main.1a24ae71.js"
+  }
+]);
