e8ca75df2b83c1266335e76db03966bd53061790
Zijian Zhou <zzhou3@ualberta.ca>
2020-03-05 03:54:53 -0700

    add a calculate button

---
+++ huangmenji/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Huangmenji } from './Huangmenji';
+
+function App() {
+  return (
+    <div className="App">
+    <Huangmenji/>
+    </div>
+  );
+}
+
+export default App;
---
+++ huangmenji/src/Huangmenji.js
@@ -0,0 +1,13 @@
+import React ,{ Component } from "react";
+import "./Cal.css"
+
+export class Huangmenji extends Component{
+    render(){
+        return(
+            <div>
+            
+            <button className="cal" >Calculate it!</button>
+            </div>
+        )
+    }
+}
